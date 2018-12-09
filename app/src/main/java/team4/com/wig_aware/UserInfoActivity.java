53b4b1435bfa324ac8e2b755dcd78ca481fde3a4
crostreet74 <crosstreet74@gmail.com>
2018-12-10 00:00:55 +0900

    fixed minor overlap of Settings

--- app/src/main/java/team4/com/wig_aware/UserInfoActivity.java
+++ app/src/main/java/team4/com/wig_aware/UserInfoActivity.java
@@ -31,8 +31,6 @@ public class UserInfoActivity extends AppCompatActivity {
 
         Toast.makeText(this, "비밀번호가 변경되었습니다", Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
         finish();
     }
 }
